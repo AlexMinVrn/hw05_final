dca742467c26311f3c9fb9bfb6a1049465c1e214
Александр Минаев <a_minaev14@mail.ru>
2022-06-26 13:38:58 +0300

    Рабочая версия. Исправлена работа pytest. Тесты работают. Преобразован словарь в кортежи.

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -40,27 +40,25 @@ class PostModelTest(TestCase):
     def test_text_label(self):
         """verbose_name поля совпадает с ожидаемым."""
         post = PostModelTest.post
-        field_verboses = {
-            'text': 'Текст поста',
-            'pub_date': 'Дата публикации',
-            'author': 'Автор',
-            'group': 'Группа',
-            'image': 'Картинка'
-        }
-        for field, expected_value in field_verboses.items():
-            with self.subTest(field=field):
-                self.assertEqual(
-                    post._meta.get_field(field).verbose_name, expected_value)
+        field_verboses = (
+            ('text', 'Текст поста'),
+            ('pub_date', 'Дата публикации'),
+            ('author', 'Автор'),
+            ('group', 'Группа'),
+            ('image', 'Картинка'),
+        )
+        for field, expected_value in field_verboses:
+            self.assertEqual(
+                post._meta.get_field(field).verbose_name, expected_value)
 
     def test_text_help_text(self):
         """help_text поля text совпадает с ожидаемым."""
         post = PostModelTest.post
-        field_help_texts = {
-            'text': 'Введите текст поста',
-            'group': 'Группа, к которой будет относиться пост',
-            'image': 'Загрузите картинку'
-        }
-        for field, expected_value in field_help_texts.items():
-            with self.subTest(field=field):
-                self.assertEqual(
-                    post._meta.get_field(field).help_text, expected_value)
+        field_help_texts = (
+            ('text', 'Введите текст поста'),
+            ('group', 'Группа, к которой будет относиться пост'),
+            ('image', 'Загрузите картинку'),
+        )
+        for field, expected_value in field_help_texts:
+            self.assertEqual(
+                post._meta.get_field(field).help_text, expected_value)
